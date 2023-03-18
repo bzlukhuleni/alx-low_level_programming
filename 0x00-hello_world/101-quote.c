2bd064201c57a29a7dd00ab524e477c6ebeff382
Banathi Lukhuleni <bzlukuleni@yahoo.co.za>
2023-03-18 02:31:16 -0700

    8 advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - main function
+ *
+ * Return: 1 in this case
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
